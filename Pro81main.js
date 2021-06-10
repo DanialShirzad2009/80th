09c0fc1d5c466b3d06a875a46690d94c4f076dee
DanialShirzad2009 <76796719+DanialShirzad2009@users.noreply.github.com>
2021-06-10 11:27:34 -0700

    Add files via upload

---
+++ Pro81main.js
@@ -0,0 +1,32 @@
+function getParagraph1()
+{
+var input = [];
+}
+for(var i= 1 ; i <=6 ; i++)
+{
+inputs.push(document.getElementById("para1_input_box_" + i).value);
+}
+inputs.join(". ");
+{
+document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
+}
+
+{
+document.getElementById("showParagraph2").innerHTML = inputs.join(". ");
+}
+
+{
+document.getElementById("showParagraph3").innerHTML = inputs.join(". ");
+}
+
+{
+document.getElementById("showParagraph4").innerHTML = inputs.join(". ");
+}
+
+{
+document.getElementById("showParagraph5").innerHTML = inputs.join(". ");
+}
+
+{
+document.getElementById("showParagraph6").innerHTML = inputs.join(". ");
+}
